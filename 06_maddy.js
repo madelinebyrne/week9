8b2672ece76836ac8d3faf1cadc3f1e3ef8e9fd4
Madeline Byrne <madelinebyrne@seattleacademy.org>
2016-02-12 12:59:49 -0800

    changed keys for control

---
+++ 06_maddy.js
@@ -0,0 +1,72 @@
+ThunderConnector = require('./thunder-connector');
+
+var keypress = require('keypress')
+  , tty = require('tty');
+
+// make `process.stdin` begin emitting "keypress" events
+
+ThunderConnector.connect();
+
+function up(degrees){
+	stopTime = Math.floor(degrees * 22.3);
+	setTimeout(function(){ThunderConnector.command('up');},0);
+	setTimeout(function(){ThunderConnector.command('stop');},stopTime);
+}
+
+function down(degrees){
+	stopTime = Math.floor(degrees * 22.3);
+	setTimeout(function(){ThunderConnector.command('down');},0);
+	setTimeout(function(){ThunderConnector.command('stop');},stopTime);
+}
+
+function turnRightDegrees(degrees){
+	stopTime = Math.floor(degrees * 22.3)
+	setTimeout(function(){ThunderConnector.command('right');},0);
+	setTimeout(function(){ThunderConnector.command('stop');},stopTime);
+}
+
+function turnLeftDegrees(degrees){
+	stopTime = Math.floor(degrees * 22.3)
+	setTimeout(function(){ThunderConnector.command('left');},0);
+	setTimeout(function(){ThunderConnector.command('stop');},stopTime);
+}
+
+function fire(){
+	setTimeout(function(){ThunderConnector.command('fire');},0);
+}
+
+keypress(process.stdin);
+
+// listen for the "keypress" event
+process.stdin.on('keypress', function (ch, key) {
+  console.log('got "keypress"', key);
+
+  if (key.name == 'u'){
+  	console.log("moved up");
+  	up(10);
+  } else if (key.name == 'j'){
+  	console.log("moved down");
+  	down(10);
+  } else if (key.name == 'k'){
+  	console.log("moved right");
+  	turnRightDegrees(10);
+  } else if (key.name == 'h'){
+  	console.log("moved left");
+  	turnLeftDegrees(10);
+  } else if (key.name == 'space'){
+  	console.log("fired");
+  	fire();
+  }
+
+
+  if (key && key.ctrl && key.name == 'enter') {
+    process.stdin.pause();
+  }
+});
+
+if (typeof process.stdin.setRawMode == 'function') {
+  process.stdin.setRawMode(true);
+} else {
+  tty.setRawMode(true);
+}
+process.stdin.resume();
